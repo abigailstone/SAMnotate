7c4378440d8609b80ebd9deff8cadfe11032fd82
abigailstone <abigail@abigailstone.net>
2023-10-27 18:20:28 -0400

    color conversion and segment display update

--- segments.py
+++ segments.py
@@ -10,38 +10,25 @@ import cv2
 
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 
-def show_anns(anns):
+def show_mask(mask, ax):
     """
-    show masks
-    https://github.com/facebookresearch/segment-anything/blob/main/notebooks/automatic_mask_generator_example.ipynb
+    show mask on image 
+    https://github.com/facebookresearch/segment-anything/blob/main/notebooks/predictor_example.ipynb
     """
-    if len(anns) == 0:
-        return
+    color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
 
-    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-    ax = plt.gca()
-    ax.set_autoscale_on(False)
-
-    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
-    img[:,:,3] = 0
-
-    for ann in sorted_anns:
-        m = ann['segmentation']
-        color_mask = np.concatenate([np.random.random(3), [0.35]])
-        img[m] = color_mask
-    ax.imshow(img)
-
-
-def show_image(image, masks):
+def show_image(image, mask):
     """
-    display an image and its masks
+    display an image and its mask
     """
     plt.figure()
     plt.imshow(image)
-    show_anns(masks)
+    show_mask(mask, plt.gca())
     plt.axis('off')
     plt.show()
-
 
 
 def annotate(image):
@@ -72,7 +59,7 @@ def annotate(image):
 
     for mask in masks:
 
-        show_image(image, [mask])
+        show_image(image, mask['segmentation'])
         label = input("Label: ")
 
         # label the mask
@@ -95,7 +82,8 @@ def read_image(image_path):
     dump segments into a .json
     """
 
-    img = cv2.imread(image_path)
+    img = cv2.imread(image_path) 
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # color conversion for matplotlib
 
     labels = annotate(img)
 
