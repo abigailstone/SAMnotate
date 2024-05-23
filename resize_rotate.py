dbe527dbb867e8339ab2e8aa44d69d1a62bafea8
abigailstone <abigail@abigailstone.net>
2024-05-23 18:35:10 -0400

    automatically detect if image needs rotation

--- resize_rotate.py
+++ resize_rotate.py
@@ -15,30 +15,18 @@ def resize_rotate(input_images, output_dir):
         imname = os.path.basename(image_path)
 
         img = cv2.imread(image_path)
+
+        h, w, _ = img.shape
+
+        if h > w:
+            # rotate to landscape
+            img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
+
+        # resize to 640 x 480
         resized = cv2.resize(img, (640, 480), interpolation=cv2.INTER_CUBIC)
 
-        cv2.imshow(f"{imname}", resized)
-
-        key = cv2.waitKey(0)
-
-        # if we need to rotate
-        if key & 0xFF == ord('r'):
-            rotate = True
-        else:
-            rotate = False
-
-        cv2.destroyAllWindows()
-
-        # handle rotation
-        if rotate:
-            img = cv2.rotate(img,  cv2.ROTATE_90_CLOCKWISE)
-            resized = cv2.resize(img, (640, 480), interpolation=cv2.INTER_CUBIC)
-
-            cv2.imshow(f"{imname} rotated", resized)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows() 
-
-        # write resized image 
+        # write resized image
         cv2.imwrite(os.path.join(output_dir, imname), resized)
 
 
@@ -50,11 +38,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if not os.path.exists(args.input_dir):
-        print("Invalid input directory") 
+        print("Invalid input directory")
     if not os.path.exists(args.output_dir):
         print("Invalid output directory")
     else:
         input_images = glob(f"{args.input_dir}/*")
+        input_images.sort()
 
         # check all images for resizing and rotation
         resize_rotate(input_images, args.output_dir)
